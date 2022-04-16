eaf31b4b95c372a5a7a6939d2cd684e423f5ed29
Orie Steele <orie@transmute.industries>
2022-04-16 17:58:32 -0500

    chore: test

--- src/getInputs.js
+++ src/getInputs.js
@@ -17,13 +17,16 @@ const getInputs = () => {
   const payload = getObjectOrString(core.getInput("payload"));
   const jwk = getObjectOrString(core.getInput("jwk"));
 
+  const jws = core.getInput("jws");
+
   
   return {
     resource,
     action,
     header,
     payload,
-    jwk
+    jwk,
+    jws
   };
 };
 
