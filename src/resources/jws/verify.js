db6fa43414610f2a2fb9bb61dfb3218ff0e3f4d6
Orie Steele <orie@transmute.industries>
2022-04-16 18:01:11 -0500

    chore: test

--- src/resources/jws/verify.js
+++ src/resources/jws/verify.js
@@ -4,7 +4,7 @@ const util = require('./util');
 
 const verify = async ({ jws, jwk }) => {
     const v = await util.verify({ jws, jwk })
-    core.setOutput("verified_payload", v.payload);
+    core.setOutput("verified_payload", Buffer.from(v.payload).toString());
     core.setOutput("verified_header", v.protectedHeader);
 };
 
